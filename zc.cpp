ff4ef4a2592763ebbe9ad6bcd77cae89ee5dd6cf
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-12-17 19:14:57 +0530

    Create zc.cpp

---
+++ zc.cpp
@@ -0,0 +1,24 @@
+weight  
+   password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
+                    userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
+
+Height 
+     password = user.password;
+    password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
+
+Phone Number
+     const email = user.email
+social media password
+const username = "asfhuwneferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="sergueybrp";
